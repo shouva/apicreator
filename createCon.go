924e0d342c7b635e051a1697e6dbeeb780a200af
Muhammad Bukhori Muslim <mosleim@gmail.com>
2019-12-04 04:12:42 +0700

    fix connection files

--- createCon.go
+++ createCon.go
@@ -7,40 +7,83 @@ import (
 )
 
 func createCon() string {
-	strtemplate := `
-	package database
+	var strtemplate string
+	if config.Database.Server == mySQL {
+		strtemplate = `
+			package database
 
-	import (
-		"fmt"
-	
-		"github.com/jinzhu/gorm"
-	)
-	
-	// Con : capsuling all
-	type Con struct {
-		*gorm.DB
+			import (
+				"fmt"
+			
+				"github.com/jinzhu/gorm"
+			)
+			
+			// Con : capsuling all
+			type Con struct {
+				*gorm.DB
+			}
+			
+			// Init : this called when someone call db
+			func (db *Con) Init() {
+				fmt.Println("init")
+			}
+			
+			// New :
+			func New(host, port, username, password, dbname string) (*Con, error) {
+				_db, err := gorm.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+dbname)
+				if err != nil {
+					return &Con{}, err
+				}
+				err = _db.DB().Ping()
+				if err != nil {
+					return nil, err
+				}
+				return &Con{
+					DB: _db,
+				}, nil
+			}	
+			`
+	} else if config.Database.Server == msSQL {
+
+		strtemplate = `
+			package database
+
+			import (
+				"fmt"
+			
+				"github.com/jinzhu/gorm"
+			)
+			
+			// Con : capsuling all
+			type Con struct {
+				*gorm.DB
+			}
+			
+			// Init : this called when someone call db
+			func (db *Con) Init() {
+				fmt.Println("init")
+			}
+			
+			// New :
+			func New(host, port, username, password, dbname string) (*Con, error) {
+
+				portNumber, _ := strconv.Atoi(port)
+				connectionString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s",
+								host, username, password, portNumber, dbname)
+				_db, err := gorm.Open("mysql", connectionString)
+				if err != nil {
+					return &Con{}, err
+				}
+				err = _db.DB().Ping()
+				if err != nil {
+					return nil, err
+				}
+				return &Con{
+					DB: _db,
+				}, nil
+			}	
+			`
 	}
-	
-	// Init : this called when someone call db
-	func (db *Con) Init() {
-		fmt.Println("init")
-	}
-	
-	// New :
-	func New(host, port, username, password, dbname string) (*Con, error) {
-		_db, err := gorm.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+dbname)
-		if err != nil {
-			return &Con{}, err
-		}
-		err = _db.DB().Ping()
-		if err != nil {
-			return nil, err
-		}
-		return &Con{
-			DB: _db,
-		}, nil
-	}	
-	`
 	tmpl := template.New("create api template")
 	tmpl, err := tmpl.Parse(strtemplate)
 	if err != nil {
